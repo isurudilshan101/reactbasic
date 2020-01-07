09065205664af8e82c20b3b3feeda9582123e356
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-01-08 01:53:23 +0530

    comment 1

---
+++ react_basic/src/component/Greet.js
@@ -0,0 +1,22 @@
+ import React from 'react';
+
+//  function Greet()
+//  {
+//      return <h1>Hello isuru</h1>
+//  }
+
+const Greet =() =>
+       <div>
+            <h1>isuru dilshan</h1>
+            <h1>my classs</h1>
+       </div>
+      
+
+       
+        //<h2>my name</h2>
+        
+
+
+    
+
+ export default Greet
